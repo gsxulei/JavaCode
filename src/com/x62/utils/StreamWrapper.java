ca7bd5f35b5049c5d42d43b8aa031abd1c16d877
徐雷 <31539634+gsxulei@users.noreply.github.com>
2018-04-22 19:43:13 +0800

    添加注释

--- src/com/x62/utils/StreamWrapper.java
+++ src/com/x62/utils/StreamWrapper.java
@@ -14,6 +14,7 @@ public class StreamWrapper
 {
 	public void wrap() throws Exception
 	{
+		//将输入流包装成按行读取输入流
 		InputStream is=null;
 		BufferedReader reader=new BufferedReader(new InputStreamReader(is));
 		reader.readLine();
